497b0f10cc28976dd2153b46c6a8abc4a38b9acc
SionNoh <vmfpsel@gmail.com>
2024-04-01 17:41:03 +0900

    Narrowing study

---
+++ TypeScript/ts-study/study-list/04-Narrowing.js
@@ -0,0 +1,28 @@
+// Narrowing
+// 1. typeof
+function func04_1(x) {
+    if (typeof x === 'string') {
+        return x + 1;
+    }
+    else {
+        return x + 1;
+    }
+}
+func04_1('123');
+func04_1(123);
+function func04_2(x) {
+    let array = [];
+    if (typeof x === 'number')
+        array[0] = x;
+    else { }
+}
+/**
+ *  Assertion(타입 덮어쓰기)
+ *  1. Narrowing 할 때 사용
+ *  2. 무슨 타입이 들어올지 100% 확실할 때 사용
+ *  3. 비상용, 디버깅용 외엔 사용 X
+ */
+function func04_3(x) {
+    let array = [];
+    array[0] = x;
+}
