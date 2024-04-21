e1f1c6d0098c74b12ac8556e549c656bd5385a12
SionNoh <vmfpsel@gmail.com>
2024-04-21 21:33:43 +0900

    제목 정렬 추가

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -3,20 +3,37 @@ import './App.css';
 
 function App() {
 
-  const [title, setTitle] = useState(["강남 맛집", "용산 맛집", "홍대 맛집"]);
+  const [title, setTitle] = useState(["강남 맛집", "홍대 맛집", "용산 맛집"]);
   const [like, setLike] = useState([0, 0, 0]);
 
+  // Array 타입의 스테이트를 변경하기 위한 방법
   const titleUpdate = (i) => {
+    // Spread opertor를 사용하여 기존 배열과 다른 배열로 deep copy 한다.
     let copy = [...title];
     copy[i] = '동내 맛집';
     setTitle(copy);
   }
-
   const upLike = (i) => {
     let copy = [...like];
     copy[i] += 1;
     setLike(copy);
   }
+
+  const ascSort = () => {
+      let copy = [...title];
+      copy.sort();
+      setTitle(copy);
+  }
+
+  const descSort = () => {
+    let copy = [...title];
+    copy.sort((a,b) => {
+      if (a > b) return -1;
+      if (a < b) return 1;
+      return 0;
+    });
+    setTitle(copy);
+}
 
   return (
     <div className="App">
@@ -24,7 +41,8 @@ function App() {
       <nav className='nav-bar'> 
         <h3>Blog!!</h3>
       </nav>
-
+      <span onClick={ ascSort }>가나다 순으로 정렬</span>　　　　　　　　
+      <span onClick={ descSort }>다나가 순으로 정렬</span>
       {
         // 배열에 있는 아이템 개수 만큼 반복하는 함수를 사용하여 게시물 출력
         title.map((item, i) => (
