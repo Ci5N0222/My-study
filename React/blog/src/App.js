4c6300a16526291879cb843e7208138669a24fab
SionNoh <vmfpsel@gmail.com>
2024-04-21 21:05:54 +0900

    state 변경 함수 study

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 
 function App() {
 
-  const [title, setTitle] = useState(["강남 우동 맛집", "용산 우동 맛집"]);
+  const [title, setTitle] = useState(["강남 맛집", "용산 맛집", "홍대 맛집"]);
+  const [like, setLike] = useState(0);
 
   return (
     <div className="App">
@@ -14,23 +15,14 @@ function App() {
 
       {
         // 배열에 있는 아이템 개수 만큼 반복하는 함수를 사용하여 게시물 출력
-        title.map(item => (
-            <section className='contents'>
-              <h4>{ item }</h4>
+        title.map((item, i) => (
+            <section className='contents' key={ i }>
+              <h4>{ item } </h4>
+              <span onClick={() => setLike(like+1) }>❤</span> { like }
               <p>2월 17일</p>
             </section>
           ))
       }
-
-      <section className='contents'>
-        <h4>{ title[0] }</h4>
-        <p>2월 17일</p>
-      </section>
-
-      <section className='contents'>
-        <h4>{ title[1] }</h4>
-        <p>2월 17일</p>
-      </section>
 
     </div>
   );
