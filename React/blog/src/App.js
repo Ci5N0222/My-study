807f8d65d0b9e9dd6e1b2cbeabb721dd9a9c7732
SionNoh <vmfpsel@gmail.com>
2024-04-21 21:20:58 +0900

    배열 setState Study

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -4,7 +4,19 @@ import './App.css';
 function App() {
 
   const [title, setTitle] = useState(["강남 맛집", "용산 맛집", "홍대 맛집"]);
-  const [like, setLike] = useState(0);
+  const [like, setLike] = useState([0, 0, 0]);
+
+  const titleUpdate = (i) => {
+    let copy = [...title];
+    copy[i] = '동내 맛집';
+    setTitle(copy);
+  }
+
+  const upLike = (i) => {
+    let copy = [...like];
+    copy[i] += 1;
+    setLike(copy);
+  }
 
   return (
     <div className="App">
@@ -17,8 +29,11 @@ function App() {
         // 배열에 있는 아이템 개수 만큼 반복하는 함수를 사용하여 게시물 출력
         title.map((item, i) => (
             <section className='contents' key={ i }>
-              <h4>{ item } </h4>
-              <span onClick={() => setLike(like+1) }>❤</span> { like }
+              <h4>
+                { item } 
+                <span onClick={() => titleUpdate(i) }>수정</span>
+              </h4>
+              <span onClick={() => upLike(i) }>❤</span> { like[i] }
               <p>2월 17일</p>
             </section>
           ))
