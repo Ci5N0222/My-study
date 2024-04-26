81628506cadddab471dce163ef54b4635c46dced
SionNoh <vmfpsel@gmail.com>
2024-04-26 23:29:38 +0900

    navbar

--- nodeJS/FORUM/server.js
+++ nodeJS/FORUM/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static(__dirname + '/public'));
+
 app.listen(8080, () => {
     console.log("http://localhost:8080 에서 서버 실행중");
 });
